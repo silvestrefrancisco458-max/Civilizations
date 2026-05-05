0bef5dd3c3b286f459efbbffe41f36e57c793ffc
Luis-Portero <lporteroutrilla01.cf@iesesteveterradas.cat>
2026-05-05 18:03:22 +0200

    3
    Añadido las excepciones

---
+++ M03_Programación/src/BuildingException.java
@@ -0,0 +1,5 @@
+public class BuildingException extends Exception{
+	public BuildingException(String message){
+		super(message);
+	}
+}
---
+++ M03_Programación/src/ResourceException.java
@@ -0,0 +1,5 @@
+public class ResourceException extends Exception{
+    public ResourceException(String message) {
+        super(message);
+    }
+}
