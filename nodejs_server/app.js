d36a67aea21b5b7f7a7923f4609c3ed1a16151eb
Francisco Silvestre <silvestrefrancisco458@gmail.com>
2026-05-09 15:01:00 +0200

    cambios

---
+++ nodejs_server/app.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const path = require("path");
+const db = require("../db/database");
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", async (req, res) => {
+  try {
+    const [batalles] = await db.query(
+      "SELECT * FROM batalles ORDER BY data DESC LIMIT 2"
+    );
+
+    res.render("index", { batalles });
+  } catch (error) {
+    res.send("Error en la página principal: " + error.message);
+  }
+});
+
+app.get("/batalles", async (req, res) => {
+  try {
+    const [batalles] = await db.query(
+      "SELECT * FROM batalles ORDER BY data DESC"
+    );
+
+    const [total] = await db.query(
+      "SELECT COUNT(*) AS total FROM batalles"
+    );
+
+    res.render("batalles", {
+      batalles,
+      total: total[0].total
+    });
+  } catch (error) {
+    res.send("Error en batalles: " + error.message);
+  }
+});
+
+app.get("/informe", async (req, res) => {
+  try {
+    const id = req.query.informe;
+
+    const [batalla] = await db.query(
+      "SELECT * FROM batalles WHERE id = ?",
+      [id]
+    );
+
+    res.render("informe", { batalla: batalla[0] });
+  } catch (error) {
+    res.send("Error en informe: " + error.message);
+  }
+});
+
+app.get("/civilitzacio", async (req, res) => {
+  try {
+    const [recursos] = await db.query(
+      "SELECT * FROM recursos"
+    );
+
+    res.render("civilitzacio", { recursos });
+  } catch (error) {
+    res.send("Error en civilitzacio: " + error.message);
+  }
+});
+
+app.get("/programadors", (req, res) => {
+  res.render("programadors");
+});
+
+const httpServer = app.listen(port, () => {
+  console.log(`Servidor iniciado en: http://localhost:${port}`);
+});
+
+process.on("SIGTERM", shutDown);
+process.on("SIGINT", shutDown);
+
+function shutDown() {
+  console.log("Servidor apagado correctamente");
+  httpServer.close();
+  process.exit(0);
+}
