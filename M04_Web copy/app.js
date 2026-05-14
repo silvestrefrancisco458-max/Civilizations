f59107f40f3e61b380a9976330e153714cd45122
Francisco Silvestre <silvestrefrancisco458@gmail.com>
2026-05-14 19:38:23 +0200

    f

--- M04_Web copy/app.js
+++ M04_Web copy/app.js
@@ -5,7 +5,7 @@ const db = require("./db/database");
 const app = express();
 const port = 3000;
 
-app.set("view engine", "ejs");
+app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "./views"));
 
 app.use(express.static(path.join(__dirname, "./public")));
