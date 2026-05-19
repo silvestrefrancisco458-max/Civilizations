5e8840a1ba0505260021f39bce218817e8f0076a
Francisco Silvestre <silvestrefrancisco458@gmail.com>
2026-05-19 19:58:54 +0200

    k

--- M03_Java/src/core/DatabaseManager.java
+++ M03_Java/src/core/DatabaseManager.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 
 public class DatabaseManager {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/civilizations";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3307/civilizations";
     private static final String USER = "root";
     private static final String PASSWORD = "tuclave";
 
